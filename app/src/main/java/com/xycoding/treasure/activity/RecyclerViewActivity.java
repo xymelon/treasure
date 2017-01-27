5878ad21af5212405c28b002f9d3d9c02501b7f2
xuyang <cowfighting@163.com>
2017-01-27 11:08:20 +0800

    add expandable recycler view

--- app/src/main/java/com/xycoding/treasure/activity/RecyclerViewActivity.java
+++ app/src/main/java/com/xycoding/treasure/activity/RecyclerViewActivity.java
@@ -1,5 +1,7 @@
 package com.xycoding.treasure.activity;
 
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
@@ -7,13 +9,17 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.xycoding.treasure.R;
 import com.xycoding.treasure.adapter.RecyclerViewAdapter;
 import com.xycoding.treasure.databinding.ActivityRecyclerViewBinding;
+import com.xycoding.treasure.databinding.LayoutRecyclerViewItemBinding;
 import com.xycoding.treasure.rx.RxViewWrapper;
 import com.xycoding.treasure.view.LoadMoreView;
+import com.xycoding.treasure.view.recyclerview.ExpandableRecyclerViewAdapter;
 import com.xycoding.treasure.view.recyclerview.GridBottomDividerItemDecoration;
 import com.xycoding.treasure.view.recyclerview.ItemDragCallBack;
 import com.xycoding.treasure.view.recyclerview.LoadMoreRecyclerView;
@@ -47,7 +53,8 @@ public class RecyclerViewActivity extends BaseBindingActivity {
         mBinding = (ActivityRecyclerViewBinding) binding;
         initLinearRecyclerView();
         initGridRecyclerView();
-        switchRecyclerView();
+        initGroupRecyclerView();
+        recyclerViewVisible(0);
     }
 
     @Override
@@ -64,10 +71,22 @@ public class RecyclerViewActivity extends BaseBindingActivity {
                 loadMoreItems();
             }
         });
-        subscriptions.add(RxViewWrapper.clicks(mBinding.btnSwitch).subscribe(new Action1<Void>() {
+        subscriptions.add(RxViewWrapper.clicks(mBinding.btnBorder).subscribe(new Action1<Void>() {
             @Override
             public void call(Void aVoid) {
-                switchRecyclerView();
+                recyclerViewVisible(0);
+            }
+        }));
+        subscriptions.add(RxViewWrapper.clicks(mBinding.btnMore).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+                recyclerViewVisible(1);
+            }
+        }));
+        subscriptions.add(RxViewWrapper.clicks(mBinding.btnGroup).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+                recyclerViewVisible(2);
             }
         }));
     }
@@ -118,13 +137,74 @@ public class RecyclerViewActivity extends BaseBindingActivity {
                         columns));
     }
 
-    private void switchRecyclerView() {
-        if (mBinding.recyclerView.getVisibility() != View.VISIBLE) {
-            mBinding.recyclerView.setVisibility(View.VISIBLE);
-            mBinding.recyclerViewGrid.setVisibility(View.INVISIBLE);
-        } else {
-            mBinding.recyclerView.setVisibility(View.INVISIBLE);
-            mBinding.recyclerViewGrid.setVisibility(View.VISIBLE);
+    private void initGroupRecyclerView() {
+        List<ExpandableRecyclerViewAdapter.ExpandableGroup<String, String>> groups = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            List<String> children = new ArrayList<>();
+            for (int j = 1; j <= i; j++) {
+                children.add("Child : " + j);
+            }
+            String head = "Group : " + i;
+            ExpandableRecyclerViewAdapter.ExpandableGroup<String, String> group =
+                    new ExpandableRecyclerViewAdapter.ExpandableGroup<>(head, children);
+            groups.add(group);
+        }
+        final ExpandableRecyclerViewAdapter<String, String, GroupHolder> adapter =
+                new ExpandableRecyclerViewAdapter<String, String, GroupHolder>(groups) {
+                    @Override
+                    public GroupHolder onCreateHeadViewHolder(ViewGroup parent) {
+                        return new GroupHolder(
+                                LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_recycler_view_item, parent, false));
+                    }
+
+                    @Override
+                    public GroupHolder onCreateChildViewHolder(ViewGroup parent, int viewType) {
+                        return new GroupHolder(
+                                LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_recycler_view_item, parent, false));
+                    }
+
+                    @Override
+                    public void onBindHeadViewHolder(GroupHolder holder, String s, boolean expanded) {
+                        holder.binding.layoutContainer.setBackgroundColor(Color.GRAY);
+                        holder.binding.tvLabel.setText(s);
+                        holder.binding.ivSort.setImageResource(
+                                expanded ? R.drawable.ic_expand_less_black_24dp : R.drawable.ic_expand_more_black_24dp);
+                    }
+
+                    @Override
+                    public void onBindChildViewHolder(GroupHolder holder, String s) {
+                        holder.binding.tvLabel.setText(s);
+                        holder.binding.ivSort.setVisibility(View.INVISIBLE);
+                    }
+                };
+        mBinding.recyclerViewGroup.setAdapter(adapter);
+        mBinding.recyclerViewGroup.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    class GroupHolder extends RecyclerView.ViewHolder {
+
+        private LayoutRecyclerViewItemBinding binding;
+
+        public GroupHolder(View itemView) {
+            super(itemView);
+            binding = DataBindingUtil.bind(itemView);
+        }
+    }
+
+    private void recyclerViewVisible(int type) {
+        mBinding.recyclerView.setVisibility(View.INVISIBLE);
+        mBinding.recyclerViewGrid.setVisibility(View.INVISIBLE);
+        mBinding.recyclerViewGroup.setVisibility(View.INVISIBLE);
+        switch (type) {
+            case 1:
+                mBinding.recyclerView.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                mBinding.recyclerViewGroup.setVisibility(View.VISIBLE);
+                break;
+            default:
+                mBinding.recyclerViewGrid.setVisibility(View.VISIBLE);
+                break;
         }
     }
 
--- app/src/main/java/com/xycoding/treasure/view/recyclerview/ExpandableRecyclerViewAdapter.java
+++ app/src/main/java/com/xycoding/treasure/view/recyclerview/ExpandableRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.xycoding.treasure.view.recyclerview;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
@@ -10,20 +11,20 @@ import java.util.List;
 /**
  * Created by xuyang on 2017/1/23.
  */
-public abstract class ExpandableRecyclerViewAdapter<CHILD, HEAD extends ExpandableHead> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public abstract class ExpandableRecyclerViewAdapter<HEAD, CHILD, HOLDER extends RecyclerView.ViewHolder>
+        extends RecyclerView.Adapter<HOLDER> {
 
     private final static int ITEM_TYPE_HEADER = -1024;
 
-    private List<CHILD> mChildItems;
-    private List<HEAD> mHeadItems;
+    private List<? extends ExpandableGroup<HEAD, CHILD>> mGroups;
+    private OnGroupHeaderClickListener mGroupHeaderClickListener;
 
-    public ExpandableRecyclerViewAdapter(@NonNull List<CHILD> childItems, @NonNull List<HEAD> headItems) {
-        mChildItems = childItems;
-        mHeadItems = headItems;
+    public ExpandableRecyclerViewAdapter(@NonNull List<? extends ExpandableGroup<HEAD, CHILD>> groups) {
+        mGroups = groups;
     }
 
     @Override
-    public final RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public final HOLDER onCreateViewHolder(ViewGroup parent, int viewType) {
         if (viewType == ITEM_TYPE_HEADER) {
             return onCreateHeadViewHolder(parent);
         }
@@ -31,52 +32,136 @@ public abstract class ExpandableRecyclerViewAdapter<CHILD, HEAD extends Expandab
     }
 
     @Override
-    public final void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public final void onBindViewHolder(HOLDER holder, int position) {
+        int currentPosition = -1;
+        for (ExpandableGroup<HEAD, CHILD> group : mGroups) {
+            currentPosition++;
+            if (currentPosition == position) {
+                onBindHeadViewHolder(holder, group.getHead(), group.isExpanded());
+                onGroupHeaderClick(holder, group);
+                return;
+            }
+            if (group.isExpanded()) {
+                for (CHILD child : group.getChildren()) {
+                    currentPosition++;
+                    if (currentPosition == position) {
+                        onBindChildViewHolder(holder, child);
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     @CallSuper
     @Override
     public int getItemViewType(int position) {
+        if (isGroupHeader(position)) {
+            return ITEM_TYPE_HEADER;
+        }
         return super.getItemViewType(position);
     }
 
     @Override
     public final int getItemCount() {
-        return mChildItems.size() + mHeadItems.size();
-    }
-
-    public abstract RecyclerView.ViewHolder onCreateHeadViewHolder(ViewGroup parent);
-
-    public abstract RecyclerView.ViewHolder onCreateChildViewHolder(ViewGroup parent, int viewType);
-
-    public abstract void onBindHeadViewHolder(RecyclerView.ViewHolder holder, int position);
-
-    public abstract void onBindChildViewHolder(RecyclerView.ViewHolder holder, int position);
-
-    private int childPositionToExpandablePosition(int position) {
-        int offset = 0, headSize = mHeadItems.size();
-        for (int i = 0; i < headSize; i++) {
-            if (mHeadItems.get(i).getPosition() > position) {
-                break;
+        int count = 0;
+        for (ExpandableGroup group : mGroups) {
+            count++;
+            if (group.isExpanded()) {
+                count += group.getChildren().size();
             }
-            ++offset;
         }
-        return position + offset;
+        return count;
     }
 
-//    private int expandablePositionToChildPosition(int sectionedPosition) {
-//        if (isSectionHeaderPosition(sectionedPosition)) {
-//            return RecyclerView.NO_POSITION;
-//        }
-//        int offset = 0;
-//        for (int i = 0; i < mSections.size(); i++) {
-//            if (mSections.valueAt(i).sectionedPosition > sectionedPosition) {
-//                break;
-//            }
-//            --offset;
-//        }
-//        return sectionedPosition + offset;
-//    }
+    /**
+     * @param flatPosition the flat position (raw index within the list of visible items in the RecyclerView)
+     * @return
+     */
+    public boolean isGroupHeader(int flatPosition) {
+        int currentPosition = -1;
+        for (ExpandableGroup group : mGroups) {
+            currentPosition++;
+            if (currentPosition == flatPosition) {
+                return true;
+            }
+            if (group.isExpanded()) {
+                currentPosition += group.getChildren().size();
+            }
+            if (currentPosition >= flatPosition) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private void onGroupHeaderClick(final HOLDER holder, final ExpandableGroup<HEAD, CHILD> group) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                group.expanded = !group.isExpanded();
+                onBindHeadViewHolder(holder, group.head, group.expanded);
+                if (group.expanded) {
+                    notifyItemRangeInserted(holder.getAdapterPosition() + 1, group.children.size());
+                } else {
+                    notifyItemRangeRemoved(holder.getAdapterPosition() + 1, group.children.size());
+                }
+                if (mGroupHeaderClickListener != null) {
+                    mGroupHeaderClickListener.onGroupHeaderClick(group.expanded);
+                }
+            }
+        });
+    }
+
+    public void setGroupHeaderClickListener(OnGroupHeaderClickListener listener) {
+        this.mGroupHeaderClickListener = listener;
+    }
+
+    public abstract HOLDER onCreateHeadViewHolder(ViewGroup parent);
+
+    public abstract HOLDER onCreateChildViewHolder(ViewGroup parent, int viewType);
+
+    public abstract void onBindHeadViewHolder(HOLDER holder, HEAD head, boolean expanded);
+
+    public abstract void onBindChildViewHolder(HOLDER holder, CHILD child);
+
+    public interface OnGroupHeaderClickListener {
+        void onGroupHeaderClick(boolean expanded);
+    }
+
+    /**
+     * Created by xuyang on 2017/1/23.
+     */
+    public static class ExpandableGroup<HEAD, CHILD> {
+
+        private HEAD head;
+        private boolean expanded;
+        private List<CHILD> children;
+
+        public ExpandableGroup(@NonNull HEAD head, @NonNull List<CHILD> children) {
+            this.head = head;
+            this.expanded = false;
+            this.children = children;
+        }
+
+        public ExpandableGroup(@NonNull HEAD head, @NonNull List<CHILD> children, boolean expanded) {
+            this.head = head;
+            this.children = children;
+            this.expanded = expanded;
+        }
+
+        public HEAD getHead() {
+            return head;
+        }
+
+        public List<CHILD> getChildren() {
+            return children;
+        }
+
+        public boolean isExpanded() {
+            return expanded;
+        }
+
+    }
 
 }
